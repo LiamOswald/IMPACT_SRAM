246a3626dc048606f15f0ea5b318d21dca66a8e3
LiamOswald <wdo1621@jagmail.southalabama.edu>
2023-02-17 15:08:46 -0600

    Begining Branch, this will get standard SRAM into the Caravel

---
+++ docker_fix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DOCKER_SWAP=$(pwd)/mgmt_core_wrapper
+export DOCKER_MEMORY=$(pwd)/mgmt_core_wrapper
+export ROUTING_CORES=$(pwd)/mgmt_core_wrapper
+export STD_CELL_LIBRARY=$(pwd)/mgmt_core_wrapper
+
